4fcebc69f61afb4a089eacd118abcef66857b899
Monojit Datta <datta.monojit@gmail.com>
2020-12-16 00:35:34 +0530

    Update aksauto-cluster-postconfig.ps1

--- Deployments/Setup/aksauto-cluster-postconfig.ps1
+++ Deployments/Setup/aksauto-cluster-postconfig.ps1
@@ -43,6 +43,7 @@ Invoke-Expression -Command $nginxNSCommand
 $nginxRepoAddCommand = "helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx"
 Invoke-Expression -Command $nginxRepoAddCommand
 
+
 $nginxRepoUpdateCommand = "helm repo update"
 Invoke-Expression -Command $nginxRepoUpdateCommand
 
